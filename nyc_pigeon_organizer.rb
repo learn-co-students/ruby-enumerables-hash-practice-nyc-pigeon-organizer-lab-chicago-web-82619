0e5ba062b1dac43ddf83b016f946fdc20102201a
Justice Marks <her@shedaddy.com>
2019-07-25 23:13:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  pigeon_list = {}
+  
+  data.each do |quality, types|
+    types.each do |type, pigeon_names| 
+      pigeon_names.each do |name|
+        
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        
+        if !pigeon_list[name][quality]
+          pigeon_list[name][quality] = []
+        end
+        
+        pigeon_list[name][quality].push(type.to_s)
+        
+        
+        
+      end
+     
+      end
+    
+    end
+  
+  return pigeon_list
+  
+  
+  
 end
