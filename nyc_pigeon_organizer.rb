205c3c6451b50b011cb2bdbc8ef4b78173fb7e07
Zachary Flowers <zflowers0@gmail.com>
2019-08-18 17:49:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons = {}
+  data[:lives].each do |el, i|
+    i.each do |name|
+      pigeons[name] = {:color => [], :gender => [], :lives => []}
+    end
+  end
+  
+  data.values.each do |el|
+    el.each do |i|
+      pigeons.keys.each do |name|
+        if i[1].include?(name)
+          pigeons[name][data.keys[0]].push(i.first.to_s)
+          binding.pry
+        end
+      end
+    end
+  end
+  pigeons
 end
