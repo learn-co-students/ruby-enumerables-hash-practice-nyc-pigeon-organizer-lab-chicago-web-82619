5e24de4d52171a28ff421b91ffe4ef58aa6cd5af
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-23 20:23:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.reduce({}) { |hash, (features, values)|
+    values.reduce(hash) { |hash, (feature, pigeons)|
+      pigeons.each { |pigeon|
+        if !hash[pigeon]
+          hash[pigeon] = {}
+        end
+        if hash[pigeon][features]
+          hash[pigeon][features].push("#{feature}")
+        else
+          hash[pigeon][features] = ["#{feature}"]
+        end
+        hash
+      }
+      hash
+    }
+    hash
+  }
 end
